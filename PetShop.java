bc7463c76bfd253fc1d4eebb6a176dd42ea0a19f
matiaszo <tiaszuniga@gmail.com>
2024-09-18 15:20:34 -0300

    falta arrumar a funcao de eliminar tutor

--- PetShop.java
+++ PetShop.java
@@ -35,7 +35,7 @@ public class PetShop {
             int age = tutor.getAge();
 
             String address = tutor.getAddress();
-            System.out.println("Codigo do tutor:" + id);
+            System.out.println("Codigo do tutor: " + id);
             System.out.println("    Nome: " + name);
             System.out.println("    Data nascimento: " + dayBirth + "/" + monthBirth + "/" + yearBirth + " (" + age + " anos)");
             System.out.println("    Endereco: " + address);
@@ -76,7 +76,11 @@ public class PetShop {
     }
 
     public void deleteTutor(int tutor_id){
-        tutors.remove(tutor_id);
+        Tutor tutor_eliminate = this.getTutors().get(tutor_id);
+        if (tutor_eliminate.getId() > 0) {
+            
+            tutors.remove(tutor_id);
+        }
 }
 
     public static void clearBuffer(Scanner scanner){
@@ -87,7 +91,7 @@ public class PetShop {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int tutor_id = 0;
         PetShop petshop = new PetShop();
         while (true) {
             
@@ -96,12 +100,6 @@ public class PetShop {
         String option = scanner.nextLine();
 
         switch (option) {
-
-            case "b":
-                System.out.println("Digite o codigo do tutor a ser localizado: ");
-                int tutor_id = scanner.nextInt();
-                clearBuffer(scanner);
-                petshop.searchPet(tutor_id);
 
             case "c":
                 while (true) {
@@ -180,16 +178,38 @@ public class PetShop {
                 petshop.printRegister();
                 System.out.println("----------------------------------------------");
                 System.out.println();
+                break;
 
+            case "b":
+                System.out.println("Digite o codigo do tutor a ser localizado: ");
+                tutor_id = scanner.nextInt();
+                clearBuffer(scanner);
+                petshop.searchPet(tutor_id);
+                break;
+
+            case "e":
+                System.out.print("Digite o codigo do tutor a ser excluido: ");
+                tutor_id = scanner.nextInt();
+                clearBuffer(scanner);
+
+                if (petshop.getTutors().size() < tutor_id) {
+                    System.out.println("Nao existe um tutor com o id " + tutor_id);
+                    break;
+                }
+
+                petshop.deleteTutor(tutor_id);
+                System.out.println("--- Tutor (+pets) com codigo"+ tutor_id + "excluido com sucesso! ---");
+                System.out.println();
+                break;
             case "x":
-            System.out.println("Saindo");
-            System.out.println();
-
-            break;  
+                System.out.println("Saindo");
+                System.out.println();
+                break;
 
             default:
             System.out.println("No default");
             System.out.println();
+            break;
         }
         System.out.println("aqui");
     }
